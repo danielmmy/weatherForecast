cc578c8d5611b58c427b04d7b9be4d30f536e3e0
danielmmy <danielmmy10@gmail.com>
2019-11-06 21:17:51 -0200

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+console.log('From the javascript file')
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#messageOne')
+const messageTwo = document.querySelector('#messageTwo')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    const address = search.value
+
+    if(address === ''){
+        return messageOne.textContent = 'You must submit a location'
+    }
+
+    fetch('http://localhost:3000/weather?address=' + address).then( (response) => {
+        response.json().then( (data) => {
+            if(data.err){
+                return messageOne.textContent = data.err
+            }
+            messageOne.textContent = data.forecast
+            messageTwo.textContent = data.location
+        } )
+    } )
+})
---
+++ src/app.js
@@ -0,0 +1,81 @@
+const path = require('path')
+
+const express = require('express')
+const hbs = require('hbs')
+
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname,'../templates/views'))
+hbs.registerPartials(path.join(__dirname,'../templates/partials'))
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('', (req, res) => {
+    res.render('index',{
+        title: 'Weather',
+        name: 'Danielmmy'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about',{
+        title: 'About',
+        name: 'Danielmmy'
+    })
+})
+
+app.get('/help', (req, res) =>{
+    res.render('help',{
+        title: 'Help',
+        name: 'Danielmmy'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send( {
+            error: 'An address must be provided'
+        })
+    }
+
+    geocode(req.query.address, (err, {latitude, longitude, location} = {} ) => {
+        if(err){
+            return res.send({err})
+        }
+        forecast(latitude, longitude, (err, forecastData) => {
+            if(err){
+                return res.send({err})
+            }
+            
+            return res.send( { 
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })  
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('error',{
+        title: 'Help Error Page',
+        name: 'Danielmmy',
+        error: 'Help article not found'
+    })
+})
+
+
+app.get('*', (req, res) => {
+    res.render('error',{
+        title: 'Error Page',
+        name: 'Danielmmy',
+        error: 'Page not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
