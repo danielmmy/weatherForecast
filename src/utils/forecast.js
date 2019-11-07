f51157477da32c2657b5d0e709672ed1e39c5750
danielmmy <danielmmy10@gmail.com>
2019-11-07 07:17:58 -0200

    Added info to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,14 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/05687e8e3ba4b216803a33dd4f485bbd/' + latitude + ',' + longitude + '?units=si&lang=pt'
+    const url = 'https://api.darksky.net/forecast/05687e8e3ba4b216803a33dd4f485bbd/' + latitude + ',' + longitude + '?units=si'
     request({ url, json: true}, (err, {body} = {} ) => {
         if(err){
             callback('Unable to connect to weather service',undefined)
         }else if(body.error){
             callback('Unable to find wheater information',undefined)
         }else{
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' celsius degrees out. There is a '+ body.currently.precipProbability+ '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' celsius degrees out. There is a '+ body.currently.precipProbability+ '% chance of rain.' + ' Daily high is ' + body.daily.data[0].temperatureHigh + ' and low is '+ body.daily.data[0].temperatureLow + '.')
         }
     })
 }
